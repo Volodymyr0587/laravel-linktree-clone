02c3eab780ff72c446348e492dada1a8143049e3
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-04 22:43:52 +0200

    display user's links on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,25 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+            <div class="p-6 text-gray-900">
+                <div class="grid grid-cols-4 gap-4">
+                    @foreach ($links as $link)
+                    <div>
+                        <a href="{{ $link->link }}" target="_blank"
+                            style="background-color: {{ $link->user->background_color }}"
+                            class="block max-w-sm p-6 border border-gray-200 rounded-lg shadow">
+
+                            <h5 style="color: {{ $link->user->text_color }}"
+                                class="mb-2 text-2xl font-bold tracking-tight">{{ $link->name }}</h5>
+                            <p class="font-normal text-gray-700">{{ $link->visits->count() }}</p>
+                        </a>
+                    </div>
+                    @endforeach
+
                 </div>
             </div>
+
         </div>
     </div>
 </x-app-layout>
